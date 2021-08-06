ddc6510d84efb46f9d7ce8485f382686de41baae
abashkatov <as@bashkatov.pro>
2021-08-06 21:09:37 +0300

    Пример работы с потоками FileStream

---
+++ ConsoleApp1/FileSystem/StreamExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+
+namespace ConsoleApp1.FileSystem
+{
+    class StreamExample
+    {
+        public void Run() 
+        {
+            using (Stream stream = new FileStream("./test.txt", FileMode.Truncate))
+            {
+                string Test = "вапрвапвап"; // windows-1251
+                // utf-8
+                byte[] Bytes = Encoding.Unicode.GetBytes(Test);
+                //byte[] BadBytes = { 250 };
+                //stream.Write(BadBytes, 0, 1);
+                stream.Write(Bytes, 0, Bytes.Length);
+            }
+            using (Stream stream = new FileStream("./test.txt", FileMode.Open))
+            {
+                long len = stream.Length;
+                byte[] Bytes = new byte[len];
+                int readed = stream.Read(Bytes, 0, Bytes.Length);
+
+                //WriteLine(Encoding.Unicode.GetString(Bytes));
+                WriteLine(Encoding.Unicode.GetString(Bytes, 0, readed));
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ConsoleApp1.FileSystem;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,47 +11,12 @@ namespace ConsoleApp1
 {
     class Program
     {
-        class Person {
-            public delegate void OnPropertyChanged<T>(T oldValue, T newValue);
-            public Person(string name, int age)
-            {
-                Name = name;
-                Age = age;
-            }
-            public event OnPropertyChanged<string> onNameChanged = null;
-            public event OnPropertyChanged<int> onAgeChanged = null;
-            private string _name;
-            private int _age;
-            public string Name {
-                get => _name;
-                set
-                {
-                    string oldName = _name;
-                    _name = value;
-                    onNameChanged?.Invoke(oldName, value);
-                }
-            }
-            public int Age
-            {
-                get => _age;
-                set
-                {
-                    int oldAge = _age;
-                    _age = value;
-                    onAgeChanged?.Invoke(oldAge, value);
-                }
-            }
-        }
         static void Main(string[] args)
         {
-            Person person;
-            person = new Person("Alex", 33);
-            person.Name = "Butch";
-            person.onNameChanged += (string oldName, string newName) => {
-                WriteLine($"Имя изменилось: {oldName} => {newName}");
-            };
 
-            person.Name = "John";
+
+            var Example = new StreamExample();
+            Example.Run();
 
             ReadKey();
         }
