32b8a5d526c18931d03241792b0e6e9196bd8397
abashkatov <as@bashkatov.pro>
2021-08-13 19:55:59 +0300

    пример парсинга XML

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml;
 using System.Xml.Serialization;
 using static System.Console;
 
@@ -12,74 +14,29 @@ namespace ConsoleApp1
 {
     public class Program
     {
-        [Serializable]
-        public class Person {
-            public Person()
-            { }
-            public Person(string name, int age)
-            {
-                Name = name;
-                Age = age;
-            }
-            public string Name { get; set; }
-            public int Age { get; set; }
-            [NonSerialized]
-            public List<Person> People  = new List<Person>();
-        }
-
         static void Main(string[] args)
         {
-            Person
-                p1 = new Person("Name1", 33),
-                p2 = new Person("Name2", 44),
-                p3 = new Person("Name3", 55),
-                p4 = new Person("Name4", 66),
-                p5 = new Person("Name5", 77);
-            p1.People.Add(p2);
-            p1.People.Add(p3);
-            p2.People.Add(p4);
-            p2.People.Add(p5);
-            List<Person> Persons = new List<Person>() { p1 , p2 , p3 , p4 , p5 };
-            List<Person> Persons2;
-            // Json
-            // XML
-            // SOAP
-
-            // Бинарный
-
-            // Yaml
-
-            // Перерыв до 20-54
-
-            IFormatter formatter = new BinaryFormatter();
-            formatter = new SoapFormatter();
-            String fileName = "data.soap";
-
-            JsonSerializer jsonSerializer = new JsonSerializer();
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Person>));
-
-            using (Stream fs = new FileStream("data.json", FileMode.OpenOrCreate))
+            using (XmlTextReader reader = new XmlTextReader("data.xml")) 
             {
-                using(JsonWriter jsonWriter = new JsonTextWriter(new StreamWriter(fs)))
+                reader.WhitespaceHandling = WhitespaceHandling.None;
+                while (reader.Read()) 
                 {
-                    jsonSerializer.Serialize(jsonWriter, Persons);
+                    WriteLine("LineNumber " + reader.LineNumber);
+                    WriteLine("Depth " + reader.Depth);
+                    WriteLine("Name " + reader.Name);
+                    WriteLine("NodeType " + reader.NodeType);
+                    WriteLine("ValueType " + reader.ValueType.Name);
+                    WriteLine("Value " + reader.Value);
+                    WriteLine("AttributeCount " + reader.AttributeCount);
+                    if (reader.AttributeCount > 0) 
+                    {
+                        while (reader.MoveToNextAttribute()) 
+                        {
+                            WriteLine($"\t{reader.NodeType}, {reader.Name}, {reader.Value}");
+                        }
+                    }
+                    WriteLine("==================");
                 }
-            }
-            using (Stream fs = new FileStream("data.xml", FileMode.OpenOrCreate))
-            {
-                xmlSerializer.Serialize(fs, Persons);
-            }
-            Persons = null;
-            using (Stream fs = new FileStream("data.json", FileMode.Open))
-            {
-                using (JsonReader jsonReader = new JsonTextReader(new StreamReader(fs)))
-                {
-                    Persons = jsonSerializer.Deserialize(jsonReader, typeof(List<Person>)) as List<Person>;
-                }
-            }
-            using (Stream fs = new FileStream("data.xml", FileMode.Open))
-            {
-                Persons2 = xmlSerializer.Deserialize(fs) as List<Person>;
             }
 
             ReadKey();
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadLine();
+        }
+    }
+}
